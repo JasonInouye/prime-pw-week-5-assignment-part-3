312d258412a518914524a5605c88c85c9cbdb8f5
Jason Inouye <jasonjinouye@gmail.com>
2022-02-18 14:33:09 -0600

    created collection array and addToColleciton function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+
+// create an array collection
+let collection = [];
+
+let album0 ={
+    title: 'Purple Rain',
+    artist:'Prince',
+    yearPublished:'1996'
+}   // end album
+
+function addToCollection(title, artist, yearPublished){
+    let album0 ={
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }   // end album
+    console.log( album0 );
+    collection.push( `album0` );
+    console.log( collection );
+} // End addToCollection
