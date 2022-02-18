df4a30ed2fed0e3efe72ee6aa6ecb179b9427d38
Jason Inouye <jasonjinouye@gmail.com>
2022-02-18 14:40:18 -0600

    Added log of test of attToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,11 +3,14 @@ console.log('***** Music Collection *****')
 // create an array collection
 let collection = [];
 
+// Test album object
+/*
 let album0 ={
     title: 'Purple Rain',
     artist:'Prince',
     yearPublished:'1996'
 }   // end album
+*/
 
 function addToCollection(title, artist, yearPublished){
     let album0 ={
@@ -15,7 +18,10 @@ function addToCollection(title, artist, yearPublished){
         artist: artist,
         yearPublished: yearPublished
     }   // end album
-    console.log( album0 );
+    // console.log( album0 );
     collection.push( `album0` );
     console.log( collection );
+    return album0;
 } // End addToCollection
+
+console.log( addToCollection(`The Essential Bob Dylan`, `Bob Dylan`, `2016`) );
