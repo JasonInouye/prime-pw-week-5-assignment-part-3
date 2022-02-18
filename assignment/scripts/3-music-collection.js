44ce8d156072026d1d005bf4bf526d925293c238
Jason Inouye <jasonjinouye@gmail.com>
2022-02-18 15:08:31 -0600

    Push 6 different albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,15 +13,22 @@ let album0 ={
 */
 
 function addToCollection(title, artist, yearPublished){
-    let album0 ={
+    var albumName = title;
+    console.log( `The album name is `, albumName );
+    var albumName ={
         title: title,
         artist: artist,
         yearPublished: yearPublished
-    }   // end album
-    // console.log( album0 );
-    collection.push( `album0` );
-    console.log( collection );
-    return album0;
+    }   // end albumName
+    console.log( `The album name to be pushed is`, title );
+    collection.push( title );
+    //console.log( collection );
+    return albumName;
 } // End addToCollection
 
 console.log( addToCollection(`The Essential Bob Dylan`, `Bob Dylan`, `2016`) );
+console.log( addToCollection(`Legend the best of Bob Marley`, `Bob Marley`, `1984`) );
+console.log( addToCollection(`Purple Rain`, `Prince`, `1984`) );
+console.log( addToCollection(`21`, `Adele`, `2011`) );
+console.log( addToCollection(`25`, `Adele`, `2015`) );
+console.log( addToCollection(`From a Room Vol.1 and Vol.2`, `Chris Stapleton`, `2017`) );
